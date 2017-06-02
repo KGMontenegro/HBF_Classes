08491d7f3f5362bebded55311decc103d71a0c7a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-01 17:04:49 -0700

    melons initial commit

---
+++ melons1.py
@@ -0,0 +1,46 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder(object):
+
+    def __init__(self, species, qty, country_code=0):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = "domestic"
+        self.tax = 0.08
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+        super(InternationalMelonOrder, self).__init__(species, qty)
+        self.country_code = country_code
+        self.order_type = "international"
+        self.tax = 0.17
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
